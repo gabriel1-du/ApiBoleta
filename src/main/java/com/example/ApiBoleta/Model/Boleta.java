e6643921adf9739765fc7dadc334baf95a9e1901
Gabriel Duran <gabrieldurans999@gmail.com>
2025-05-26 20:08:22 -0400

    Primer commit : primera version del modelo

---
+++ src/main/java/com/example/ApiBoleta/Model/Boleta.java
@@ -0,0 +1,61 @@
+package com.example.ApiBoleta.Model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CollectionId;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "boleta")
+public class Boleta {
+
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "boleta_id")
+    private Integer boletaId;
+
+    @Column(name = "subtotal", nullable = false, length =  13)
+    private Integer subtotal;
+
+    @Column(name = "impuesto", nullable = false, length = 13)
+    private Integer impuesto;
+
+    @Column(name = "total", nullable = false, length = 13)
+    private Integer total;
+
+    @Column(name = "fecha_emision", nullable = false)
+    private LocalDateTime fecha_emision;
+
+
+
+    //Relacion con otras clases/entidades
+    //Relacion con la clase pedido
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedido pedido;
+
+    //Relacion con la clase usuario
+    @Column(name = "usuario_id", nullable = false)
+    private Integer usuarioId;
+
+    
+
+
+}
---
+++ src/main/java/com/example/ApiBoleta/Model/Pedido.java
@@ -0,0 +1,56 @@
+package com.example.ApiBoleta.Model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Pedido")
+@Data
+public class Pedido {
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pedido_id")
+    private Integer id;
+
+    @Column(name = "fecha_pedido", nullable = false)
+    private LocalDateTime fechaPedido;
+
+    @Column(name = "total", nullable = false)
+    private Integer total;
+
+    @Column(name = "estado", nullable = false, length = 30)
+    private String estado;
+
+    //Relacion Entidad/Clase
+
+    //Relacion con la clase boleta
+    @OneToMany 
+    @JoinColumn(name = "boleta", nullable = false)
+    private Boleta boleta;
+
+    
+
+    
+
+
+
+     
+    
+
+}
